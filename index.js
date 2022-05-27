d11c3f0f2ba7b8da59c48468a670c785be5131b5
26fromthebar <kostas.gelinio@gmail.com>
2022-05-27 22:47:37 +0300

    refactored lightbox to be more reusable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.thumbsSwiper', {
   spaceBetween: 10,
-  slidesPerView: '4',
+  slidesPerView: 'auto',
   freeMode: true,
   watchSlidesProgress: true,
 });
@@ -8,33 +8,37 @@ const swiper = new Swiper('.thumbsSwiper', {
 const swiper2 = new Swiper('.gallerySwiper', {
   spaceBetween: 10,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.lightbox-btn-next',
+    prevEl: '.lightbox-btn-prev',
   },
   thumbs: {
     swiper: swiper,
   },
 });
 
-const openBtn = document.querySelector('.btn-lightbox-open');
-const closeBtn = document.querySelector('.btn-lightbox-close');
+const openBtn = document.querySelector('.lightbox-btn-open');
+const closeBtn = document.querySelector('.lightbox-btn-close');
+const swiperWrapper = document.querySelector('.swiper-wrapper.ltbox');
+const body = document.querySelector('body');
 
-openBtn.addEventListener('click', () => {
-  const lightbox = document.querySelector('.lightbox');
-  lightbox.classList.add('lightbox-open');
-  lightbox.classList.remove('lightbox-close');
+if (swiperWrapper.children.length !== 0) {
+  openBtn.addEventListener('click', () => {
+    const lightbox = document.querySelector('.lightbox');
+    lightbox.classList.add('lightbox-open');
+    lightbox.classList.remove('lightbox-close');
 
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-      lightbox.classList.add('lightbox-close');
-      lightbox.classList.remove('lightbox-open');
-    }
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        lightbox.classList.add('lightbox-close');
+        lightbox.classList.remove('lightbox-open');
+      }
+    });
   });
-});
 
-closeBtn.addEventListener('click', () => {
-  const lightbox = document.querySelector('.lightbox');
+  closeBtn.addEventListener('click', () => {
+    const lightbox = document.querySelector('.lightbox');
 
-  lightbox.classList.add('lightbox-close');
-  lightbox.classList.remove('lightbox-open');
-});
+    lightbox.classList.add('lightbox-close');
+    lightbox.classList.remove('lightbox-open');
+  });
+}
